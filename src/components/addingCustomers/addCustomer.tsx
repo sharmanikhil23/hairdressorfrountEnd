bf8c63e2a6488dc8a84279e317f91a231c98f350
Nikhil Sharma <nikhil23062000@icloud.com>
2023-08-14 09:15:32 -0400

    made some chnages in page 2

--- src/components/addingCustomers/addCustomer.tsx
+++ src/components/addingCustomers/addCustomer.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./css/addCustomer.css";
 import { FirstAddCustomerForm } from "./forms/firstForm/firstForm";
@@ -8,14 +8,36 @@ type addCustomerFormNumber = {
   addCustomerFormNumber: number;
 };
 
+export type user = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  hPhone: string;
+  wPhone: string;
+  birthday: string;
+};
+
+const initialVal: user = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  hPhone: "",
+  wPhone: "",
+  birthday: "",
+};
 export const AddCustomers = (props: addCustomerFormNumber) => {
   const { addCustomerFormNumber } = props;
+  const [user, setUser] = useState<user>(initialVal);
+
   return (
     <>
       <div id="beforeAddCustomerFormAreaStart">
         <div className="applicationWidth">
           {addCustomerFormNumber === 1 ? (
-            <FirstAddCustomerForm></FirstAddCustomerForm>
+            <FirstAddCustomerForm
+              user={user}
+              setUser={setUser}
+            ></FirstAddCustomerForm>
           ) : (
             <SecondAddCustomerForm></SecondAddCustomerForm>
           )}
--- src/components/addingCustomers/forms/firstForm/firstForm.tsx
+++ src/components/addingCustomers/forms/firstForm/firstForm.tsx
@@ -1,36 +1,112 @@
 import React from "react";
 
+import { user } from "../../addCustomer";
+
 import "./css/firstForm.css";
 
-export const FirstAddCustomerForm = () => {
+type prop = {
+  user: user;
+  setUser: React.Dispatch<React.SetStateAction<user>>;
+};
+
+let todayDate = new Date().toISOString().split("T")[0];
+
+export const FirstAddCustomerForm = (props: prop) => {
+  let { user, setUser } = props;
   return (
     <div id="firstAddCustomerForm">
       <section id="firstAndLastName">
         <section className="customerAddFormElmAlighn">
-          <label>Last Name</label>
-          <input type="text" className="customerAddFormInput"></input>
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            type="text"
+            className="customerAddFormInput"
+            id="lastName"
+            name="lastName"
+            onChange={(e) => {
+              setUser((current: user): user => {
+                let data = { ...current, lastName: e.target.value };
+                return data;
+              });
+            }}
+            value={user.lastName}
+          ></input>
         </section>
         <section className="customerAddFormElmAlighn">
-          <label>First Name</label>
-          <input type="text" className="customerAddFormInput"></input>
+          <label htmlFor="firstName">First Name</label>
+          <input
+            type="text"
+            className="customerAddFormInput"
+            id="firstName"
+            name="firstName"
+            onChange={(e) => {
+              setUser((current: user): user => {
+                let data = { ...current, firstName: e.target.value };
+                return data;
+              });
+            }}
+            value={user.firstName}
+          ></input>
         </section>
       </section>
       <section id="birthday" className="customerAddFormElmAlighn">
-        <label>Birthday</label>
-        <input type="date" className="customerAddFormInput"></input>
+        <label htmlFor="birthday">Birthday</label>
+        <input
+          placeholder="yyyy-mm-dd"
+          type="date"
+          className="customerAddFormInput"
+          id="birthday"
+          name="birthday"
+          max={todayDate}
+          onChange={(e) => {
+            setUser((current: user): user => {
+              let data = { ...current, birthday: e.target.value };
+              return data;
+            });
+          }}
+          value={user.birthday}
+        ></input>
       </section>
       <section id="email" className="customerAddFormElmAlighn">
-        <label>Email</label>
-        <input type="email" className="customerAddFormInput"></input>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          className="customerAddFormInput"
+          id="email"
+          name="email"
+          onChange={(e) => {
+            setUser((current: user): user => {
+              let data = { ...current, email: e.target.value };
+              return data;
+            });
+          }}
+          value={user.email}
+        ></input>
       </section>
       <section id="multiplePhn">
         <section className="customerAddFormElmAlighn">
-          <label>Phone (Home)</label>
-          <input type="tel" className="customerAddFormInput"></input>
+          <label htmlFor="hPhone">Phone (Home)</label>
+          <input
+            type="tel"
+            className="customerAddFormInput"
+            id="hPhone"
+            name="hPhone"
+            onChange={(e) => {
+              setUser((current: user): user => {
+                let data = { ...current, hPhone: e.target.value };
+                return data;
+              });
+            }}
+            value={user.hPhone}
+          ></input>
         </section>
         <section className="customerAddFormElmAlighn">
           <label>Phone (Work)</label>
-          <input type="tel" className="customerAddFormInput"></input>
+          <input
+            type="tel"
+            className="customerAddFormInput"
+            value={user.wPhone}
+          ></input>
         </section>
       </section>
     </div>
